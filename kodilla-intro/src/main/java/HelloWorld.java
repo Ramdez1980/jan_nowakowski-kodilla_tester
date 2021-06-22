8fda69cb715c391f33bfedeef8373326fbdcf150
Jan Nowakowski <jan.nowakowski@poczta.onet.pl>
2021-06-22 16:48:28 +0200

    Hello World

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!");
+        System.out.println("JAN");
+        System.out.println("NOWAKOWSKI");
+        System.out.println('N');
+    }
+}
