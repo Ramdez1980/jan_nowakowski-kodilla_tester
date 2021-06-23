fc807355f76b2c95ab7b99f1e62af793e02d501d
Jan Nowakowski <jan.nowakowski@poczta.onet.pl>
2021-06-23 13:02:59 +0200

    new Main and MainExtend file

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,19 @@
+public class MainExtended
+{
+    public static void main(String[] args)
+    {
+        String firstName = "Jan";
+        String Surname = "Nowakowski";
+        int age = 40;
+        double height = 1.75;
+        boolean job = true;
+        String jobCompany = "Warsaw Stock Exchange";
+
+        System.out.println(firstName);
+        System.out.println(Surname);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(job);
+        System.out.println(jobCompany);
+    }
+}
