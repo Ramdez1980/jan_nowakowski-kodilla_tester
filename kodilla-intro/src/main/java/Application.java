ec59621a48e08c2ae0d72ca5c62ab964ad110412
Jan Nowakowski <jan.nowakowski@poczta.onet.pl>
2021-07-18 10:58:07 +0200

    „KOD-2.6

---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,12 @@
+public class Application {
+
+    public static void main(String[] args) {
+
+        ApplicationHelper  adam = new ApplicationHelper ("Adam", 25 , 178);
+        adam.checkAgeHeight();
+
+
+
+    }
+
+}
