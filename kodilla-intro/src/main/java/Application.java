a06871a07adce3d7982caa928a4504dee5ecc53f
Jan Nowakowski <jan.nowakowski@poczta.onet.pl>
2021-07-19 16:08:37 +0200

    2.6  Refaktoryzacja kodu

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -2,7 +2,7 @@ public class Application {
 
     public static void main(String[] args) {
 
-        ApplicationHelper  adam = new ApplicationHelper ("Adam", 25 , 178);
+        ApplicationHelper  adam = new ApplicationHelper ("Adam  ", 25 , 178);
         adam.checkAgeHeight();
 
 
--- kodilla-intro/src/main/java/ApplicationHelper.java
+++ kodilla-intro/src/main/java/ApplicationHelper.java
@@ -11,12 +11,12 @@ public class ApplicationHelper {
     }
 
     public void checkAgeHeight() {
-        if (this.age > 30 && this.height > 160) {
-            System.out.println("User is older than 30 and higher than 160cm");
-        } else {
-            System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+        if (/*name.length() != 0*/ name != null) {
+            if (this.age > 30 && this.height > 160) {
+                System.out.println("User is older than 30 and higher than 160cm");
+            } else {
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+            }
         }
     }
 }
-
-
