5e2f72d29f3edecea7dbeb5775e49b8ca5a105a3
Jan Nowakowski <jan.nowakowski@poczta.onet.pl>
2021-08-12 13:41:40 +0200

    KOD-3.1

--- kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/OperatingSystem.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/OperatingSystem.java
@@ -4,20 +4,20 @@ public class OperatingSystem {
 
     private int YearOfPublication;
 
-    public void turnOn()
-    {
+    public void turnOn() {
         System.out.println("System is turned on");
     }
 
-    public void turnOff(){
+    public void turnOff() {
         System.out.println("System is turned off");
     }
 
     public OperatingSystem(int YearOfPublication) {
-        this.YearOfPublication =  YearOfPublication;
+        this.YearOfPublication = YearOfPublication;
     }
 
     public int getYearOfPublication() {
         return YearOfPublication;
     }
+
 }
